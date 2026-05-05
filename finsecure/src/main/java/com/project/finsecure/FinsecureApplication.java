f02327fda055b91bb5d1dcf679ae6389bfc2c801
kishoreravi0804 <kishoreravi0804@gmail.com>
2026-05-06 00:15:00 +0530

    fixed Dockerfile name

--- finsecure/src/main/java/com/project/finsecure/FinsecureApplication.java
+++ finsecure/src/main/java/com/project/finsecure/FinsecureApplication.java
@@ -3,23 +3,16 @@ package com.project.finsecure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.DataSourceTransactionManagerAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class, 
-    DataSourceTransactionManagerAutoConfiguration.class, 
-    HibernateJpaAutoConfiguration.class 
-})
+
+@SpringBootApplication
 public class FinsecureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinsecureApplication.class, args);
 		 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("Admin@123"));
-		System.out.println("hello");
+       
 	}
 
 }
